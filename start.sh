0268ed748b6c612fb0803171eeb405dec3dcf039
DaviSM <davimesquita@gmail.com>
2021-04-22 16:43:18 -0300

    rodar todas

--- start.sh
+++ start.sh
@@ -31,24 +31,62 @@ function sim2() {
   --strategy $3 \
   --use_fee_asset BNB \
   --buy_pct $1 \
-  --currency_capital 2000 \
+  --currency_capital 1000 \
   --filename /data/db/$2-$1-$3.html --non_interactive
 
+}
+function sim3() {
+    sim2 BTT 10 $1
+    sim2 BTC 10 $1
+}
+function all() {
+  sim3 bollinger
+  sim3 cci_srsi
+  sim3 crossover_vwap
+  sim3 dema
+  sim3 ehlers_ft
+  sim3 ehlers_mama
+  sim3 ehlers_trend
+  sim3 ichimoku
+  sim3 ichimoku_score
+  sim3 kc
+  sim3 macd
+  sim3 momentum
+  sim3 multi
+  sim3 neural
+  sim3 noop
+  sim3 pivot
+  sim3 renko
+  sim3 rsi
+  sim3 sar
+  sim3 speed
+  sim3 srsi_macd
+  sim3 stddev
+  sim3 ta_ema
+  sim3 ta_macd
+  sim3 ta_macd_ext
+  sim3 ta_ppo
+  sim3 ta_srsi_bollinger
+  sim3 ta_stoch_bollinger
+  sim3 ta_trix
+  sim3 ta_ultosc
+  sim3 ti_bollinger
+  sim3 ti_hma
+  sim3 ti_stoch
+  sim3 ti_stoch_bollinger
+  sim3 trend_bollinger
+  sim3 trend_ema
+  sim3 trendline
+  sim3 trust_distrust
+  sim3 vpt
+  sim3 wavetrend
 }
 function main() {
   mongo_start
 
   back BTT
   back BTC
-  sim 10 BTT ta_macd 1.5 2.5
-  sim 10 BTC ta_macd 1.5 2.5
-  sim 25 BTT ta_macd 1.5 2.5
-  sim 25 BTC ta_macd 1.5 2.5
-
-  sim2 10 BTT ta_macd
-  sim2 10 BTC ta_macd
-  sim2 25 BTT ta_macd
-  sim2 25 BTC ta_macd
+  all
 
   mongo_exit
 }
